d8f47282c36790a1fa5156587c75c6f5180f8421
Nima <alireza.khazforoosh@adventistas.org>
2024-01-03 10:48:40 -0300

    search issues: 1-searching works on next page 2-no result found

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,14 +9,29 @@
             @include('shared.success-message')
             @include('shared.submit-idea')
             <hr>
-            @foreach ($ideas as $idea)
-                <div class="mt-3">
-                    @include('shared.idea-card')
-                </div>
-            @endforeach
+            @if (count($ideas) > 0)
+                @foreach ($ideas as $idea)
+                    <div class="mt-3">
+                        @include('shared.idea-card')
+                    </div>
+                @endforeach
+            @else
+                No results found!
+            @endif
+            {{-- ! 
+                ! another way of doing above:
+                todo: @forelse ($ideas as $idea )
+                todo:      <div class="mt-3">
+                todo:          @include('shared.idea-card')
+                todo:      </div>
+                todo: @empty
+                todo:   <p class="text-center mt-4">No results found!</p>
+                todo: @endforelse   
+            ! --}}
             <div class="mt-3">
                 {{-- this part downside is for showing the paginate arrows(pagination bottons), to go to different pages --}}
-                {{ $ideas->links() }}
+                {{-- withquerystring() will inject query strings to the URL: it is for searching times, when you search and go to next page it will help to have the search --}}
+                {{ $ideas->withQueryString()->links() }}
             </div>
         </div>
         <div class="col-3">
--- resources/views/shared/search-bar.blade.php
+++ resources/views/shared/search-bar.blade.php
@@ -5,7 +5,9 @@
     <div class="card-body">
         <form action="{{ route('dashboard') }}" method="get">
             {{-- ! for the form to work we always need the name for the element that wants to send data to URL --}}
-            <input placeholder="..." name="search" class="form-control w-100" type="text">
+            {{-- ! value is for when we search and go to next page still having the search on the search bar --}}
+            <input value="{{ request('search', '') }}" placeholder="..." name="search" class="form-control w-100"
+                type="text">
             <button type="submit" class="btn btn-dark mt-2"> Search</button>
         </form>
     </div>
