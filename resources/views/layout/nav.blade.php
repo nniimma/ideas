ad86074d1c1596b1d4ee1fe0ac37dce08a7bd7c7
Nima <alireza.khazforoosh@adventistas.org>
2024-01-05 18:07:08 -0300

    active menu links

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,7 +1,8 @@
 <nav class="navbar navbar-expand-lg bg-dark border-bottom border-bottom-dark ticky-top bg-body-tertiary"
     data-bs-theme="dark">
     <div class="container">
-        <a class="navbar-brand fw-light" href="/"><span class="fas fa-brain me-1"> </span> {{ env('APP_NAME') }} </a>
+        <a class="navbar-brand fw-light" href="/"><span class="fas fa-brain me-1"> </span> {{ env('APP_NAME') }}
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -11,17 +12,20 @@
                 {{--  ! guest is oposite of auth and it means if the user is not loged it show the content inside --}}
                 @guest
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('login') }}">Login</a>
+                        <a class="{{ Route::is('login') ? 'active' : '' }} nav-link" aria-current="page"
+                            href="{{ route('login') }}">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">Register</a>
+                        <a class="{{ Route::is('register') ? 'active' : '' }} nav-link"
+                            href="{{ route('register') }}">Register</a>
                     </li>
                 @endguest
                 {{-- ! auth means if just the user is loged in show the content inside --}}
                 @auth
                     <li class="nav-item">
                         {{-- ! to acces the user's information on laravel we can use Auth class --}}
-                        <a class="nav-link" href="{{ route('profile') }}">{{ Auth::user()->name }}</a>
+                        <a class="{{ Route::is('profile') ? 'active' : '' }} nav-link"
+                            href="{{ route('profile') }}">{{ Auth::user()->name }}</a>
                     </li>
                     <li class="nav-item">
                         <form action="{{ route('logout') }}" method="post">
--- resources/views/shared/left-sidebar.blade.php
+++ resources/views/shared/left-sidebar.blade.php
@@ -2,32 +2,21 @@
     <div class="card-body pt-3">
         <ul class="nav nav-link-secondary flex-column fw-bold gap-2">
             <li class="nav-item">
-                <a class="nav-link text-dark" href="#">
+                {{-- ! {{ (route::is('dashboard')) ? '' : '' }} means if it is on dashboard page do the first and if not do the second one: --}}
+                <a class="{{ Route::is('dashboard') ? 'text-white bg-primary rounded' : '' }} nav-link"
+                    href="{{ route('dashboard') }}">
                     <span>Home</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <span>Explore</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <span>Feed</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
+                {{-- ! we can give the url and not the name: Request::is('terms') --}}
+                <a class="{{ Request::is('terms') ? 'text-white bg-primary rounded' : '' }} nav-link"
+                    href="{{ url('terms') }}">
                     <span>Terms</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <span>Support</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <span>Settings</span></a>
             </li>
         </ul>
     </div>
-    <div class="card-footer text-center py-2">
-        <a class="btn btn-link btn-sm" href="#">View Profile </a>
+    <div class="{{ Route::is('profile') ? 'bg-primary rounded' : '' }} card-footer text-center py-2">
+        <a class="{{ Route::is('profile') ? 'text-white' : '' }} btn btn-link btn-sm" href="{{ route('profile') }}">View
+            Profile </a>
     </div>
 </div>
